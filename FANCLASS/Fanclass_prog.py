8e2a8728d0eba188d29832f61248926c3e802810
jptmaz <jaspagmaz98@gmail.com>
2023-07-04 11:02:55 +0800

    Three Constants added

--- FANCLASS/Fanclass_prog.py
+++ FANCLASS/Fanclass_prog.py
@@ -1,5 +1,9 @@
 #Design a class named fan to represent a FAN.
+class Fan():
     #Create Three Constants (SLOW, MEDIUM, & FAST) with 3 values (1, 2, & 3) to denote the fan speed.
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
         #Private int data field for speed
         #Private bool data field that specifies whether or not the fan is on(Default is False)
         #Private string data field that specifies fan color
