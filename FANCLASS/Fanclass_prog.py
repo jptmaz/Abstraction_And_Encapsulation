b4f9edd6a9c1e1228feb103972277e3ba936cbca
jptmaz <jaspagmaz98@gmail.com>
2023-07-04 13:35:12 +0800

    FAN CLASS program completed

--- FANCLASS/Fanclass_prog.py
+++ FANCLASS/Fanclass_prog.py
@@ -4,7 +4,38 @@ class Fan():
     SLOW = 1
     MEDIUM = 2
     FAST = 3
-        #Private int data field for speed
-        #Private bool data field that specifies whether or not the fan is on(Default is False)
+    #Constructor that creates fan with specified speed, radius, color, and on (default false)
+    def __init__(self, speed = SLOW, radius = 5, color = "BLUE", on="FALSE"):    
+        #Private int data that specifies speed of fan
+        self.speed = speed
+        # Private bool data field that specifies whether or not the fan is on(Default is False)
+        self.status = on
         #Private string data field that specifies fan color
+        self.color = color
+        #Private bool data that specifies radius
+        self.radius = radius
+        
     #Accessor(getter) and mutator(seters) on all four data fields
+    def get_speed(self):
+        return self.speed
+    
+    def get_status(self):
+        return self.status
+    
+    def get_radius(self):
+        return self.radius
+    
+    def get_color(self):
+        return self.color
+    
+    def set_speed(self, updated_speed):
+        self.speed = updated_speed
+    
+    def set_status(self, updated_status):
+        self.status = updated_status
+    
+    def set_radius(self, updated_status):
+        self.get_radius
+    
+    def set_color(self, updated_color):
+        self.get_color
