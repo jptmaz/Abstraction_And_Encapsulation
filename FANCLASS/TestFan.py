b883ae879180f0a9ba85c11cd4e8ac65cc50d28b
jptmaz <jaspagmaz98@gmail.com>
2023-07-04 14:26:54 +0800

    Program is functioning properly

--- FANCLASS/TestFan.py
+++ FANCLASS/TestFan.py
@@ -1,29 +1,25 @@
 from Fanclass_prog import Fan
 
-class Test_Fan:
-    Fan = Fan
-    
-    def FC_Test_Run(self):
-        self.fan_1 = Fan(Fan.FAST, 10, "YELLOW", "TRUE")
-        self.fan_2 = Fan(Fan.MEDIUM, 5, "BLUE", "FALSE")
+class TestFan():
+    def fc_test_run(self):
+        self.Fan_1 = Fan(Fan.FAST, 10, "YELLOW", True)
+        self.Fan_2 = Fan(Fan.MEDIUM, 5, "BLUE", False)
         
-    def Test_Run_Stat(self):
-        print(''' WELCOME TO FAN PROG!
-              Let's check the status of your two fans.
-              ''')
+        print("""Welcome to Fan prog!
+    Let's see the status of your 2 fans!""")
         
         print(" ------- ||| FAN #1 ||| -------")
-        print("Status: ", self.fan_1.get_status())
-        print("Speed: ", self.fan_1.get_speed())
-        print("Radius: ", self.fan_1.get_radius())
-        print("Color: ", self.fan_1.get_color())
+        print("Status: ", self.Fan_1.get_status())
+        print("Speed: ", self.Fan_1.get_speed())
+        print("Radius: ", self.Fan_1.get_radius())
+        print("Color: ", self.Fan_1.get_color())
         
         print(" ------- ||| FAN #2 ||| -------")
-        print("Status: ", self.fan_2.get_status())
-        print("Speed: ", self.fan_2.get_speed())
-        print("Radius: ", self.fan_2.get_radius())
-        print("Color: ", self.fan_2.get_color())
-        
+        print("Status: ", self.Fan_2.get_status())
+        print("Speed: ", self.Fan_2.get_speed())
+        print("Radius: ", self.Fan_2.get_radius())
+        print("Color: ", self.Fan_2.get_color())
 
-run = Test_Fan
-run.Test_Run_Stat()
+
+run = TestFan()
+run.fc_test_run()
