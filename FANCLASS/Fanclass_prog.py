d8c97c105521280a277136e3e620a0f8a2c562ab
jptmaz <jaspagmaz98@gmail.com>
2023-07-04 10:57:36 +0800

    FANCLASS folder added w/ program

---
+++ FANCLASS/Fanclass_prog.py
@@ -0,0 +1,6 @@
+#Design a class named fan to represent a FAN.
+    #Create Three Constants (SLOW, MEDIUM, & FAST) with 3 values (1, 2, & 3) to denote the fan speed.
+        #Private int data field for speed
+        #Private bool data field that specifies whether or not the fan is on(Default is False)
+        #Private string data field that specifies fan color
+    #Accessor(getter) and mutator(seters) on all four data fields
