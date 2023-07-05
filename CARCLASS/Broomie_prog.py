423408d97d860fc5b5ce069bb794955f3873ba9b
jptmaz <jaspagmaz98@gmail.com>
2023-07-05 10:38:35 +0800

    Trying various things

---
+++ CARCLASS/Broomie_prog.py
@@ -0,0 +1,10 @@
+from Userinterface import carprog_ym
+from Userinterface import carprog_make
+
+class year_model(carprog_ym):
+    def input_year_model(self):
+        return super().year_model
+
+class make(carprog_make):
+    def input_make(self):
+        return super().make
--- CARCLASS/Userinterface.py
+++ CARCLASS/Userinterface.py
@@ -1,3 +1,9 @@
-#Create a class named User_Input
-    #Create methods to input the car's information
-    
+class carprog_ym():
+    def year_model(self):
+        year_model = str(input("Enter year and model: "))
+        return year_model
+
+class carprog_make():
+    def make(self):
+        make = str(input("Enter manufacturer: "))
+        return make
