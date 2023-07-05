505c2bd41b0e830d0254aa05d1f89c15c1813b33
jptmaz <jaspagmaz98@gmail.com>
2023-07-05 11:01:37 +0800

    Removed CARCLASS direectory to restart

--- CARCLASS/Userinterface.py
+++
@@ -1,9 +0,0 @@
-class carprog_ym():
-    def year_model(self):
-        year_model = str(input("Enter year and model: "))
-        return year_model
-
-class carprog_make():
-    def make(self):
-        make = str(input("Enter manufacturer: "))
-        return make
