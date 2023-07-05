19575e8b3f02c9426e4e95688c7e453d1edf3d41
jptmaz <jaspagmaz98@gmail.com>
2023-07-05 11:32:02 +0800

    Completed Carclass program

---
+++ CARCLASS/Carclass_prog.py
@@ -0,0 +1,17 @@
+#Write a class named Car
+#The class must have the attributes __year_model, __make, and __speed
+#The class should have an _ _init_ _ method that accepts the car’s year model and make as arguments. These values should be assigned to the object’s _ _year_model and _ _make data attributes. It should also assign 0 to the _ _speed data attribute.
+class Car():
+    def __init__(self, year_model, make, speed):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+    #The class should have the methods, accelerate, brake, and get_speed.
+    def accelerate(self):
+        self.__speed += 5
+    
+    def brake(self):
+        self.__speed -= 5
+        
+    def get_speed(self):
+        return self.__speed
