72738d077e44fa301aa8305ed0e97ac5a44903cb
jptmaz <jaspagmaz98@gmail.com>
2023-07-05 09:10:00 +0800

    Pseudocode for Carclass_prog written

--- CARCLASS/Carclass_prog.py
+++ CARCLASS/Carclass_prog.py
@@ -1,2 +1,4 @@
-#Write a class named Car that has the data attributes __year_model, __make, __speed
+#Write a class named Car with the attributes year_model, make, and speed.
+#It should have an __init__ method that  accepts the car's year model and make as arguments.
+#These values should be assigned to the object’s _ _year_model and _ _make data attributes. It should also assign 0 to the _ _speed data attribute.
 
