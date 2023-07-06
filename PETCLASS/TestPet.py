f4dc2df4b7dc9c782cb682557a9dc0a36c90f329
jptmaz <jaspagmaz98@gmail.com>
2023-07-06 11:37:20 +0800

    T-T

--- PETCLASS/TestPet.py
+++ PETCLASS/TestPet.py
@@ -9,12 +9,9 @@ def main():
         Vettie_command = input(str("= ")).upper()
         if Vettie_command == "YES":
             pet_name = pc_prog.set_name()
-            print(pc_prog.get_name())
             pet_type = pc_prog.set__animal_type()
-            print(pc_prog.get_animal_type())
             pet_age = pc_prog.set_age()
-            print(pc_prog.get_age())
-            Vettie_pet = f"Your pet, {pc_prog.get_name}, is a/an {pc_prog.get_animal_type}, and is a/an {pc_prog.get_age}"
+            Vettie_pet = f"Your pet, {pc_prog.get_name()}, is a/an {pc_prog.get_animal_type()}, and is a/an {pc_prog.get_age()}"
             print(Vettie_pet)
         elif Vettie_command == "NO":
             break
