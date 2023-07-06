b4b77d5510e57ff827b279d3cbb049be09c9c7b4
jptmaz <jaspagmaz98@gmail.com>
2023-07-06 11:42:27 +0800

    Changes to FANCLASS

--- FANCLASS/Fanclass_prog.py
+++ FANCLASS/Fanclass_prog.py
@@ -7,32 +7,32 @@ class Fan():
     #Constructor that creates fan with specified speed, radius, color, and on (default false)
     def __init__(self, speed = SLOW, radius = 5, color = "BLUE", on = False):    
         #Private int data that specifies speed of fan
-        self.speed = speed
+        self.__speed = speed
         # Private bool data field that specifies whether or not the fan is on(Default is False)
-        self.status = on
+        self.__status = on
         #Private string data field that specifies fan color
-        self.color = color
+        self.__color = color
         #Private bool data that specifies radius
-        self.radius = radius
+        self.__radius = radius
         
     #Accessor(getter) and mutator(seters) on all four data fields
     def get_speed(self):
-        return self.speed
+        return self.__speed
     
     def get_status(self):
-        return self.status
+        return self.__status
     
     def get_radius(self):
-        return self.radius
+        return self.__radius
     
     def get_color(self):
-        return self.color
+        return self.__color
     
     def set_speed(self, updated_speed):
-        self.speed = updated_speed
+        self.__speed = updated_speed
     
     def set_status(self, updated_status):
-        self.status = updated_status
+        self.__status = updated_status
     
     def set_radius(self, updated_status):
         self.get_radius
--- FANCLASS/TestFan.py
+++ FANCLASS/TestFan.py
@@ -2,23 +2,23 @@ from Fanclass_prog import Fan
 
 class TestFan():
     def fc_test_run(self):
-        self.Fan_1 = Fan(Fan.FAST, 10, "YELLOW", True)
-        self.Fan_2 = Fan(Fan.MEDIUM, 5, "BLUE", False)
+        self.__Fan_1 = Fan(Fan.FAST, 10, "YELLOW", True)
+        self.__Fan_2 = Fan(Fan.MEDIUM, 5, "BLUE", False)
         
         print("""Welcome to Fan prog!
     Let's see the status of your 2 fans!""")
         
         print(" ------- ||| FAN #1 ||| -------")
-        print("Status: ", self.Fan_1.get_status())
-        print("Speed: ", self.Fan_1.get_speed())
-        print("Radius: ", self.Fan_1.get_radius())
-        print("Color: ", self.Fan_1.get_color())
+        print("Status: ", self.__Fan_1.get_status())
+        print("Speed: ", self.__Fan_1.get_speed())
+        print("Radius: ", self.__Fan_1.get_radius())
+        print("Color: ", self.__Fan_1.get_color())
         
         print(" ------- ||| FAN #2 ||| -------")
-        print("Status: ", self.Fan_2.get_status())
-        print("Speed: ", self.Fan_2.get_speed())
-        print("Radius: ", self.Fan_2.get_radius())
-        print("Color: ", self.Fan_2.get_color())
+        print("Status: ", self.__Fan_2.get_status())
+        print("Speed: ", self.__Fan_2.get_speed())
+        print("Radius: ", self.__Fan_2.get_radius())
+        print("Color: ", self.__Fan_2.get_color())
 
 
 run = TestFan()
