a8f30862f4c519550850e3273d72c67efb8526ab
jptmaz <jaspagmaz98@gmail.com>
2023-07-06 09:24:52 +0800

    Petclass_prog pseudocode written

---
+++ PETCLASS/Petclass_prog.py
@@ -0,0 +1,8 @@
+#Write class named Pet with the attributes: __name, __animal, __age.
+#Pet class should have an __init__ method that has various methods.
+    #Method that assigns value to the __name
+    #Method that assigns value to the __animal_type
+    #Method that assigns value to __age
+    #Method returns value to __name
+    #Method returns value to __animal_type
+    #Method returns value to __age
