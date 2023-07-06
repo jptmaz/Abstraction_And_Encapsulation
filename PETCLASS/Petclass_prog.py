0858b6c566056f57d7da70ed041bc08c5ed66986
jptmaz <jaspagmaz98@gmail.com>
2023-07-06 09:51:15 +0800

    Petclass_prog completed

--- PETCLASS/Petclass_prog.py
+++ PETCLASS/Petclass_prog.py
@@ -5,9 +5,30 @@ class Pet():
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
-        #Method that assigns value to the __name
-        #Method that assigns value to the __animal_type
-        #Method that assigns value to __age
-        #Method returns value to __name
-        #Method returns value to __animal_type
-        #Method returns value to __age
+    #Method that assigns value to the __name
+    def set_name(self):
+        name = str(input("Enter animal name: "))
+        self.__name = name
+    #Method that assigns value to the __animal_type
+    def set__animal_type(self):
+        animal = str(input("Enter animal type: "))
+        self.__animal_type = animal
+    #Method that assigns value to __age
+    def set_age(self):
+        age = int(input("Enter animal name: "))
+        self.__age = age
+    #Method returns value to __name
+    def get_name(self):
+        animal_name = f"Your pet's name is {self.__name}"
+        print(animal_name)
+        return self.__name
+    #Method returns value to __animal_type
+    def get_animal_type(self):
+        animal_type = f"Your pet is a {self.__animal_type}"
+        print(animal_type)
+        return self.__animal_type
+    #Method returns value to __age
+    def get_age(self):
+        animal_age = f"Your pet is {self.__age} years old."
+        print(animal_age)
+        return self.__age
