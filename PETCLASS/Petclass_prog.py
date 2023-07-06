169c416967ddd2990d87a79f3689736599638415
jptmaz <jaspagmaz98@gmail.com>
2023-07-06 09:29:33 +0800

    __init__ constructor added

--- PETCLASS/Petclass_prog.py
+++ PETCLASS/Petclass_prog.py
@@ -1,8 +1,13 @@
 #Write class named Pet with the attributes: __name, __animal, __age.
-#Pet class should have an __init__ method that has various methods.
-    #Method that assigns value to the __name
-    #Method that assigns value to the __animal_type
-    #Method that assigns value to __age
-    #Method returns value to __name
-    #Method returns value to __animal_type
-    #Method returns value to __age
+class Pet():
+    #Pet class should have an __init__ method that has various methods.
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+        #Method that assigns value to the __name
+        #Method that assigns value to the __animal_type
+        #Method that assigns value to __age
+        #Method returns value to __name
+        #Method returns value to __animal_type
+        #Method returns value to __age
