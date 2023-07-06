983c646a25b2bd67ccb138f0712dac1d94ed7e24
jptmaz <jaspagmaz98@gmail.com>
2023-07-06 11:24:38 +0800

    Sana gumana na pls

--- CARCLASS/Carobject.py
+++ CARCLASS/Carobject.py
@@ -9,4 +9,12 @@ while(Acceleration != 5):
     Broomie_Status_Acceleration = Broomie.get_speed()
     print(f"You accelerated, Broomie's speed is {Broomie_Status_Acceleration} km/h")
 
+Brake = Broomie_Status_Acceleration
+
+while(Broomie_Status_Acceleration != 0):
+    Broomie.brake()
+    Brake = Broomie_Status_Acceleration - 1
+    Broomie_Status_Brake = Broomie.get_speed()
+    print(f"You used the brakes, Broomie is now going at {Broomie_Status_Brake} km/h")
+
     
